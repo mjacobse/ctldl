68360148fb34d563d57574b1add1eaabb67b2344
mjacobse <44684927+mjacobse@users.noreply.github.com>
2023-06-07 15:38:05 +0200

    Use static_assert that's valid before CWG 2518
    https://en.cppreference.com/w/cpp/language/static_assert#Defect_reports

--- ctldl/factorize/factorize_method.hpp
+++ ctldl/factorize/factorize_method.hpp
@@ -14,12 +14,10 @@ template <FactorizeMethod method>
 auto getFactorizeMethodTag() {
   if constexpr (method == FactorizeMethod::EntryWise) {
     return FactorizeMethodEntryWise{};
-  } else if constexpr (method == FactorizeMethod::UpLooking) {
-    return FactorizeMethodUpLooking{};
   } else {
-    // Basically static_assert(false), but need to make it dependent on template
-    // parameter, otherwise it always fires.
-    static_assert(method != method, "Invalid FactorizeMethod.");
+    static_assert(method == FactorizeMethod::UpLooking,
+                  "Invalid FactorizeMethod.");
+    return FactorizeMethodUpLooking{};
   }
 }
 
