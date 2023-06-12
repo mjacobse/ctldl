3e0f8efc01753c85dba5804f61034db900ea29f8
mjacobse <44684927+mjacobse@users.noreply.github.com>
2023-06-12 10:17:05 +0200

    Make entry equality operator less general
    With both parameters being template types, it fit overload resolution
    for basically any comparison. Should probably SFINAE this away. For now
    it helps to just have one parameter use a template type though.

--- ctldl/sparsity/entry.hpp
+++ ctldl/sparsity/entry.hpp
@@ -9,8 +9,10 @@ struct Entry {
   std::size_t col_index;
 };
 
-template <class EntryLhs, class EntryRhs>
-constexpr bool operator==(const EntryLhs& lhs, const EntryRhs& rhs) {
+template <class OtherEntry>
+constexpr bool operator==(const Entry& lhs, const OtherEntry& rhs) {
+  // template parameter OtherEntry is so that we can also compare to any
+  // user-type that has row_index and col_index
   return lhs.row_index == rhs.row_index && lhs.col_index == rhs.col_index;
 }
 
