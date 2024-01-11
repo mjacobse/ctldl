6c3c3752396577323fe3e342ed6b5c96b0621e1e
mjacobse <44684927+mjacobse@users.noreply.github.com>
2024-01-11 13:16:59 +0100

    Allow filling entire matrix with values

--- ctldl/factorize/fill_with_original_matrix.hpp
+++ ctldl/factorize/fill_with_original_matrix.hpp
@@ -18,6 +18,14 @@ template <std::size_t entry_index, class Matrix, class FactorData>
       getMatrixValueAt<entry_orig.row_index, entry_orig.col_index>(input));
 }
 
+template <std::size_t i, class Matrix, class FactorData>
+[[gnu::always_inline]] inline auto fillDiagonalWithOriginalMatrixValue(
+    const Matrix& input, FactorData& fact) {
+  using Value = typename FactorData::Value;
+  constexpr auto i_orig = FactorData::origRowIndex(i);
+  fact.D[i] = static_cast<Value>(getMatrixValueAt<i_orig, i_orig>(input));
+}
+
 template <std::size_t... EntryIndices, class Matrix, class FactorData>
 [[gnu::always_inline]] inline auto fillRowWithOriginalMatrixValues(
     const Matrix& input, FactorData& fact,
@@ -32,6 +40,34 @@ template <std::size_t i, class Matrix, class FactorData>
   constexpr auto row_end = FactorData::sparsity.row_begin_indices[i + 1];
   fillRowWithOriginalMatrixValues(input, fact,
                                   makeIndexSequence<row_begin, row_end>());
+}
+
+template <std::size_t... RowIndices, class Matrix, class FactorData>
+void fillWithOriginalMatrixValues(const Matrix& input, FactorData& fact,
+                                  std::index_sequence<RowIndices...>) {
+  (fillRowWithOriginalMatrixValues<RowIndices>(input, fact), ...);
+}
+
+template <std::size_t... RowIndices, class Matrix, class FactorData>
+void fillWithOriginalMatrixValuesIncludingDiagonal(
+    const Matrix& input, FactorData& fact, std::index_sequence<RowIndices...>) {
+  (fillRowWithOriginalMatrixValues<RowIndices>(input, fact), ...);
+  (fillDiagonalWithOriginalMatrixValue<RowIndices>(input, fact), ...);
+}
+
+template <class Matrix, class FactorData>
+void fillWithOriginalMatrixValues(const Matrix& input, FactorData& fact) {
+  constexpr auto num_rows = std::size_t{FactorData::sparsity.num_rows};
+  fillWithOriginalMatrixValues(input, fact,
+                               std::make_index_sequence<num_rows>());
+}
+
+template <class Matrix, class FactorData>
+void fillWithOriginalMatrixValuesIncludingDiagonal(const Matrix& input,
+                                                   FactorData& fact) {
+  constexpr auto num_rows = std::size_t{FactorData::sparsity.num_rows};
+  fillWithOriginalMatrixValuesIncludingDiagonal(
+      input, fact, std::make_index_sequence<num_rows>());
 }
 
 }  // namespace ctldl
