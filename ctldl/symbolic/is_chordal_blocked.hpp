aefe855ff62b0df6f4d18d227d3df2679455baf7
Marcel Jacobse <mjacobse@uni-bremen.de>
2026-06-06 20:26:16 +0200

    Simplify is_chordal_blocked (#17)

--- ctldl/symbolic/is_chordal_blocked.hpp
+++ ctldl/symbolic/is_chordal_blocked.hpp
@@ -3,19 +3,19 @@
 #include <ctldl/sparsity/is_lower_triangle.hpp>
 #include <ctldl/sparsity/is_sparsity_equal.hpp>
 #include <ctldl/sparsity/is_square.hpp>
+#include <ctldl/sparsity/sparsity_csr.hpp>
 #include <ctldl/symbolic/filled_in_sparsity_blocked.hpp>
 #include <ctldl/utility/contracts.hpp>
 
 namespace ctldl {
 
-template <class Sparsity11, class Sparsity21, class Sparsity22>
-constexpr bool isChordalBlocked(const Sparsity11& sparsity11,
-                                const Sparsity21& sparsity21,
-                                const Sparsity22& sparsity22) {
+constexpr bool isChordalBlocked(const SparsityViewCSR sparsity11,
+                                const SparsityViewCSR sparsity21,
+                                const SparsityViewCSR sparsity22) {
   pre(isSquare(sparsity11));
   pre(isSquare(sparsity22));
-  static_assert(Sparsity21::numCols() == Sparsity11::numCols());
-  static_assert(Sparsity22::numRows() == Sparsity21::numRows());
+  pre(sparsity21.numCols() == sparsity11.numCols());
+  pre(sparsity22.numRows() == sparsity21.numRows());
   if (!isLowerTriangle(sparsity11) || !isLowerTriangle(sparsity22)) {
     return false;
   }
