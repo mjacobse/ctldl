14b0f93ca3227cc06c968ccdabb32275a4e5a6b6
Marcel Jacobse <mjacobse@uni-bremen.de>
2026-06-06 15:11:25 +0200

    Generalise static_sized_range
    Following P3928R0. Needs bump to C++23 as it needs P2280

--- ctldl/utility/static_sized_range.hpp
+++ ctldl/utility/static_sized_range.hpp
@@ -1,28 +1,20 @@
 #pragma once
 
-#include <array>
 #include <cstddef>
 #include <ranges>
 #include <type_traits>
+#include <utility>
 
 namespace ctldl {
 
 template <class T>
-struct range_static_size;
-
-template <class T, std::size_t n>
-struct range_static_size<std::array<T, n>>
-    : std::integral_constant<std::size_t, n> {};
-
-template <class T, std::size_t n>
-struct range_static_size<T[n]> : std::integral_constant<std::size_t, n> {};
-
-template <class T>
-inline constexpr std::size_t range_static_size_v = range_static_size<T>::value;
-
-template <class T>
-concept static_sized_range = std::ranges::sized_range<T> && requires {
-  std::integral_constant<std::size_t, range_static_size_v<T>>{};
+concept static_sized_range = std::ranges::sized_range<T> && requires(T& t) {
+  std::integral_constant<std::size_t, std::ranges::size(t)>{};
 };
+
+template <static_sized_range T>
+constexpr auto range_static_size_v = decltype([](T& t) {
+  return std::integral_constant<std::size_t, std::ranges::size(t)>{};
+}(std::declval<T&>()))::value;
 
 }  // namespace ctldl
